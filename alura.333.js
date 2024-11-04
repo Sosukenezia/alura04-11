09d46293b95ac23c36d3f05773bd5763c9c5ff12
Sosukenezia <00001126811592sp@al.educacao.sp.gov.br>
2024-11-04 07:44:40 -0300

    Add files via upload

---
+++ alura.333.js
@@ -0,0 +1,41 @@
+/* styles.css */
+
+body {
+    background-color: #f4f4f4;
+    color: #333;
+}
+
+header {
+    background: #333;
+    color: #fff;
+    padding: 1rem 0;
+    text-align: center;
+}
+
+.livros {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding: 1rem;
+}
+
+.livro {
+    background: #fff;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    margin: 1rem;
+    padding: 1rem;
+    width: 300px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+.livro img {
+    border-radius: 5px;
+}
+
+footer {
+    text-align: center;
+    padding: 1rem 0;
+    background: #333;
+    color: white;
+}
